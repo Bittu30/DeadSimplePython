623f923c99dfd49c7652689115c78104c624504e
bittu <priyratan@gmail.com>
2023-06-22 10:54:51 +0530

    Adding chapter 3 more materials

--- chapter_3.py
+++ chapter_3.py
@@ -55,36 +55,97 @@ INTEREST_RATE = 5.00
 print(-42)
 print(abs(-42))
 
-c=divmod(52,17)
+c = divmod(52, 17)
 print(c)
 
-
-#the math module
+# the math module
 
 import math
+
 print(math.pi)
 print(math.tau)
 
 print(math.inf)
 print(math.nan)
 
-distance_ft=65
-angle_def=74
+distance_ft = 65
+angle_def = 74
 
-angle_rad=math.radians(angle_def)
-height_ft=distance_ft*math.tan(angle_rad)
-height_ft=round(height_ft,2)
+angle_rad = math.radians(angle_def)
+height_ft = distance_ft * math.tan(angle_rad)
+height_ft = round(height_ft, 2)
 print(height_ft)
 
-spam=True
-eggs=False
-potatoes=None
+spam = True
+eggs = False
+potatoes = None
 
 if eggs is spam:
     print("This won't work")
 
-#the walrus operator
+# the walrus operator
 
-if(eggs :=7+5)==12:
+if (eggs := 7 + 5) == 12:
     print("We have one dozen eggs")
 print(eggs)
+
+# Strings
+
+danger = "Cuidado llamas!"
+
+print(danger)
+
+quote = 'Shout, "Cuidado llamas!"'
+
+print(quote)
+
+# raw strings
+
+print(r"I love backlashes: \ Aren't they cool?")
+# Always use raw strings for regular expression patterns
+
+# f-strings
+
+int_stock = 0
+print(f"This cheese shop has {int_stock} types of cheese")
+
+print(f"{5+5=}")
+
+answer = 42
+print(f"{answer}")
+
+# Template Strings
+
+from string import Template
+
+s = Template("$greeting,$user!")
+
+print(s.substitute(greeting="Hi", user="Jason"))
+
+# A Note on String Concatenation
+
+greeting = "Hello"
+name = "Jason"
+
+message = "".join((greeting, ", ", name, "!"))
+print(message)
+
+
+# join is faster in concatenation
+
+# Functions
+
+# Function are first class citizens and you can use them
+# like any other object
+
+def tell_joke(joke_type):
+    pass
+
+
+print(tell_joke("funny"))
+
+# Classes and Objects
+
+class Joke:
+
+
